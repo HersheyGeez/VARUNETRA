dc62083abf194f5dda51d591342f3c53975f5d97
Harsh Gupta <159681642+HersheyGeez@users.noreply.github.com>
2026-06-09 22:00:06 +0530

    Frontend Basics

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from config import Config
 from extensions import db, jwt
@@ -18,6 +19,8 @@ from routes import (
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+CORS(app)
 
 db.init_app(app)
 jwt.init_app(app)
